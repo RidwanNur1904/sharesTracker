6576849a62542fb6b34c94f7b0e33ef079557cd4
RidwanNur1904 <ridwan341@outlook.com>
2026-09-02 21:44:24 +0100

    Add project files.

---
+++ sharesTracker/MainWindow.axaml.cs
@@ -0,0 +1,33 @@
+using System;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
+
+namespace sharesTracker
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object? sender, RoutedEventArgs e)
+        {
+            if (sender is Control button)
+            {
+                var flyout = new Flyout
+                {
+                    Content = new TextBlock
+                    {
+                        Text = "Button pressed!",
+                        Margin = new Avalonia.Thickness(10)
+                    }
+                };
+
+                FlyoutBase.SetAttachedFlyout(button, flyout);
+                FlyoutBase.ShowAttachedFlyout(button);
+            }
+        }
+    }
+}
